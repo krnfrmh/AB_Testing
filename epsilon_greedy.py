653495af3793fd1e0b234cc6167e80af4f80fbd1
krnfrmh <87569650+krnfrmh@users.noreply.github.com>
2022-05-02 08:53:20 +0530

    Define constants for Epsilon greedy algo

---
+++ epsilon_greedy.py
@@ -0,0 +1,8 @@
+from __future__ import print_function, division
+from builtins import range
+import matplotlib.pyplot as plt
+import numpy as np
+
+NUM_TRIALS = 10000
+EPS = 0.1
+BANDIT_PROBABILITIES = [0.2, 0.5, 0.75]
