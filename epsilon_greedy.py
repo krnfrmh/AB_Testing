39431b730914dd99c125e9f3f02d3d1495ed1fac
krnfrmh <87569650+krnfrmh@users.noreply.github.com>
2022-05-14 23:39:20 +0530

    Pulling Bandit Arm

--- epsilon_greedy.py
+++ epsilon_greedy.py
@@ -6,3 +6,15 @@ import numpy as np
 NUM_TRIALS = 10000
 EPS = 0.1
 BANDIT_PROBABILITIES = [0.2, 0.5, 0.75]
+
+class BanditArm:
+  
+  def __init__(self, p):
+    # p: the win rate
+    self.p = p
+    self.p_estimate = 0.
+    self.N = 0 # num samples collected so far
+
+  def pull(self):
+    # draw a 1 with probability p
+    return np.random.random() < self.p
