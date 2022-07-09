09c70d83eae40be01dec8ef10fc4926ccfe55a00
krnfrmh <87569650+krnfrmh@users.noreply.github.com>
2022-07-09 10:23:46 +0530

    Setting p_estimate to a higher value

--- optimistic_initial_value.py
+++ optimistic_initial_value.py
@@ -4,3 +4,10 @@ import numpy as np
 NUM_TRIALS = 10000
 EPS = 0.1
 BANDIT_PROBABILITIES = [0.2, 0.5, 0.75]
+
+class Bandit:
+  
+  def __init__(self, p):
+    self.p = p
+    self.p_estimate = 5
+    self.N = 1
