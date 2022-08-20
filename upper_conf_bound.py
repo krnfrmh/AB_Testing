7ab77f00ac1b1fc4a397c92fc33ad54d5d0e3034
krnfrmh <87569650+krnfrmh@users.noreply.github.com>
2022-08-20 23:15:13 +0530

    Iniitialization

---
+++ upper_conf_bound.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+NUM_TRIALS = 100000
+EPS = 0.1
+BANDIT_PROBABILITIES = [0.2, 0.5, 0.75]
+
+class Bandit:
+  def __init__(self, p):
+    self.p = p # win rate
+    self.p_estimate = 0.
+    self.N = 0. 
+    
