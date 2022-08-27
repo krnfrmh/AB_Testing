2282313fcd2622924567397da1fa6d97746a1eb4
krnfrmh <87569650+krnfrmh@users.noreply.github.com>
2022-08-27 17:40:05 +0530

    Updating prob estimates

--- upper_conf_bound.py
+++ upper_conf_bound.py
@@ -10,3 +10,10 @@ class Bandit:
     self.p_estimate = 0.
     self.N = 0. 
     
+  def pull(self):
+    return np.random.random() < self.p
+
+  def update(self, x):
+    self.N += 1.
+    self.p_estimate = ((self.N - 1)*self.p_estimate + x) / self.N
+    
